3fe8ff43f3edde29ffc1c5f8381189d0c2bd7988
Axel Alvarade <axel.alvarade@hotmail.fr>
2026-09-15 15:26:59 +0200

    feat(builtins): pwd
    getcwd(NULL, 0) laisse le systeme allouer a la bonne taille, le free
    est donc a notre charge. Ecriture sur le fd 1 plutot que printf, pour
    rester correct apres un dup2 et eviter le tampon de la libc au fork.
    Retourne 0, ou 1 via print_error si getcwd echoue.
    
    Aligne aussi les 33 declarations du header sur la colonne 21:
    norminette traite toute la section comme un seul bloc, et le type
    t_resolve_status impose cette colonne a tous les prototypes.
    
    Closes #9

--- includes/minishell.h
+++ includes/minishell.h
@@ -107,12 +107,12 @@ typedef struct s_shell
 	t_cmd		*cmds;
 }	t_shell;
 
-t_env			*env_init(char **envp);
-void			env_free(t_env *env);
-char			*env_get(t_env *env, char *key);
-int				env_set(t_env **env, char *key, char *value, int exported);
-void			env_unset(t_env **env, char *key);
-char			**env_to_array(t_env *env);
+t_env				*env_init(char **envp);
+void				env_free(t_env *env);
+char				*env_get(t_env *env, char *key);
+int					env_set(t_env **env, char *key, char *value, int exported);
+void				env_unset(t_env **env, char *key);
+char				**env_to_array(t_env *env);
 
 /*executor*/
 t_resolve_status	resolve_path(t_env *env, char *name, char **out_path);
@@ -127,37 +127,38 @@ int					exec_wait(pid_t pid);
 int					apply_redirs(t_redir *redirs);
 
 /*builtins*/
-int				bi_env(t_env *env);
+int					bi_env(t_env *env);
+int					bi_pwd(void);
 
 /*utils*/
-int				print_error(char *cmd, char *arg, char *msg);
-void			shell_free(t_shell *shell);
+int					print_error(char *cmd, char *arg, char *msg);
+void				shell_free(t_shell *shell);
 
 /*parser*/
-t_cmd			*parse_tokens(t_token *tokens);
-void			cmd_list_free(t_cmd *cmds);
-t_cmd			*cmd_new(void);
-void			cmd_add_back(t_cmd **head, t_cmd *new);
-int				count_args(t_token *token);
-char			**fill_argv(t_token *token, int n);
-t_token			*skip_to_next_cmd(t_token *token);
-t_token			*token_advance(t_token *token);
-t_redir			*redir_new(t_token_type type, char *target);
-void			redir_add_back(t_redir **head, t_redir *new);
-int				attach_redirs(t_cmd *cmd, t_token *token);
+t_cmd				*parse_tokens(t_token *tokens);
+void				cmd_list_free(t_cmd *cmds);
+t_cmd				*cmd_new(void);
+void				cmd_add_back(t_cmd **head, t_cmd *new);
+int					count_args(t_token *token);
+char				**fill_argv(t_token *token, int n);
+t_token				*skip_to_next_cmd(t_token *token);
+t_token				*token_advance(t_token *token);
+t_redir				*redir_new(t_token_type type, char *target);
+void				redir_add_back(t_redir **head, t_redir *new);
+int					attach_redirs(t_cmd *cmd, t_token *token);
 
 /*lexer stuff*/
-void			token_add_back(t_token **head, t_token *new);
-t_token			*new_token(t_token_type type, char *value);
-t_token			*lex_tokenize(char *line);
-void			token_list_free(t_token *head);
-t_token_type	detect_operator(char *line, int i, int *len);
-void			append_char(char *buffer, char c);
-void			flush_buffer(t_lex *lex, t_token **head);
-void			lex_init(t_lex *lex);
-int				lex_in_quote(t_lex *lex);
-int				lex_is_quote_char(char c);
-int				lex_is_operator_char(char c);
-void			lex_handle_quote_char(t_lex *lex, char c);
+void				token_add_back(t_token **head, t_token *new);
+t_token				*new_token(t_token_type type, char *value);
+t_token				*lex_tokenize(char *line);
+void				token_list_free(t_token *head);
+t_token_type		detect_operator(char *line, int i, int *len);
+void				append_char(char *buffer, char c);
+void				flush_buffer(t_lex *lex, t_token **head);
+void				lex_init(t_lex *lex);
+int					lex_in_quote(t_lex *lex);
+int					lex_is_quote_char(char c);
+int					lex_is_operator_char(char c);
+void				lex_handle_quote_char(t_lex *lex, char c);
 
 #endif
---
+++ src/builtins/bi_pwd.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bi_pwd.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aalvard <aalvarad@student.42lausanne.ch    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/01 10:10:00 by aalvard           #+#    #+#             */
+/*   Updated: 2026/08/01 15:16:29 by aalvard          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+int	bi_pwd(void)
+{
+	char	*cwd;
+
+	cwd = getcwd(NULL, 0);
+	if (!cwd)
+		return (print_error("pwd", NULL, strerror(errno)));
+	ft_putendl_fd(cwd, 1);
+	free(cwd);
+	return (0);
+}
